ac0e46fbea998efc717a1c2a50b1168cfc9f8e81
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2022-09-29 15:21:54 +0300

    Changed database format

--- Dump20220929.sql
+++ Dump20220929.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `staff` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `staff`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: staff
@@ -14,6 +16,31 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` char(15) DEFAULT NULL,
+  `lastname` char(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'Algirdas','Brazauskas'),(2,'Dalia','Grybauskaite'),(3,'Rolandas','Paksas'),(5,'bm','bm');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `employees_projects`
@@ -38,8 +65,32 @@ CREATE TABLE `employees_projects` (
 
 LOCK TABLES `employees_projects` WRITE;
 /*!40000 ALTER TABLE `employees_projects` DISABLE KEYS */;
-INSERT INTO `employees_projects` VALUES (1,1),(1,2),(2,1);
+INSERT INTO `employees_projects` VALUES (1,1),(1,2),(2,1),(NULL,NULL),(NULL,NULL),(NULL,NULL),(NULL,NULL),(NULL,NULL),(NULL,NULL),(NULL,NULL),(NULL,NULL),(NULL,3);
 /*!40000 ALTER TABLE `employees_projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projects` (
+  `projectID` int(11) NOT NULL AUTO_INCREMENT,
+  `projectname` char(15) DEFAULT NULL,
+  PRIMARY KEY (`projectID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,'Snake 3D'),(2,'Quake 5'),(3,'File Browser');
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-27 21:37:17
+-- Dump completed on 2022-09-29 15:20:19
